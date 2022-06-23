998bfb53ef93ee33457ffff946c1eba2bedf4b39
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-23 13:46:57 -0500

    feat: boiler plate for chat

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
+const http = require("http");
+const { Server } = require("socket.io"); 
 
 // ==== Middleware ====
 app.use(express.json());
 app.use(cors());
+
+
+// ==== Chat ====
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
+
 
 
 // ==== Routes ====
