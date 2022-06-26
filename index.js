a2beb7ae2890bd2665170959b6f042e1b0e7b07e
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-26 15:58:18 -0500

    chat functionality implemented

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const http = require("http");
-const { Server } = require("socket.io"); 
+const http = require('http');
+const { Server } = require('socket.io');
+
 
 // ==== Middleware ====
 app.use(express.json());
@@ -12,28 +13,33 @@ app.use(cors());
 
 // ==== Chat ====
 const server = http.createServer(app);
+const CHATPORT = process.env.CHAT_PORT || 8081
+
+server.listen(CHATPORT, () => {
+  console.log(`Chat server on ${CHATPORT}`)
+})
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room", (data) => {
+io.on('connection', (socket) => {
+    // console.log(`User Connected: ${socket.id}`);
+    // socket.on defines events
+    socket.on('join_room', (data) => {
       socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+
+    socket.on('send_message', (data) => {
+      socket.to(data.room).emit('receive_message', data);
     });
   
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
+    socket.on('disconnect', () => {
+      // console.log('User Disconnected', socket.id);
     });
   });
 
@@ -46,7 +52,7 @@ io.on("connection", (socket) => {
 
 
 // ==== Server start ====
-const PORT = process.env.PORT || 8080
+const PORT = process.env.SERVER_PORT || 8080 
 app.listen(PORT, () => {
-    console.log('Server is listening');
+    console.log(`Server is listening to ${PORT}`);
 })
