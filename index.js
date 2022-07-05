608829af615eead372f0ac622d6b704347320e96
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-05 16:52:21 -0500

    chat working

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const http = require('http');
-// const socketio = require('socket.io');
-
-const { Server } = require('socket.io');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app)
+const cors = require('cors');
+const { Server } = require('socket.io');
+require('dotenv').config();
+
 const PORT = process.env.PORT || 8080
 
 // ==== Middleware ====
@@ -15,37 +14,53 @@ app.use(cors());
 
 // // test route to test server
 const testRoute = require('./routes/testRoute')
-app.use(testRoute)
+app.use('/', testRoute)
 
 // ==== Database Routes ====
 const userRoute = require('./routes/userRoute')
 app.use('/users', userRoute)
 
 // ====== Chat ======
-const server = http.createServer(app)
-// const io = socketio(server, {cors: {origin: '*'}})
-const io = new Server(server, {cors: {origin: '*'}})
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
 
 io.on('connection', (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-  
-  socket.on('join', ({ username, room }) => {
-    socket.join(room);
-    console.log(`${username} joined`)
-    // socket.emit('message', { user: 'admin', text: `Welcome ${username}`})
-    socket.broadcast.to(room).emmit('message', { user: 'admin', text: `${username} has joined.`})
-  });
+  console.log(`User Connected: ${socket.id}`)
 
-  socket.on('send_message', (data) => {
-    socket.to(data.room).emit('receive_message', data);
-    // io.to(data.room).emit('message', data)
-    // socket.broadcast.emit('receive_message', data)
-  });
+  socket.on('disconnect', () => console.log(`User Disconnected: ${socket.id}`))
 
-  socket.on('disconnect', () => console.log('User Disconnected', socket.id));
+  socket.on('join', ({ name, room }) => {
+    socket.join(room)
+    console.log(`User ${name} joined room ${room} `)
+  })
+
+  socket.on('sendMessage', (data) => {
+    socket.to(data.room).emit('receiveMessage', data)
+
+  })
+
+  // socket.on('join', ({ username, room }) => {
+  //   socket.join(room);
+  //   console.log(`${username} joined`)
+  //   // socket.emit('message', { user: 'admin', text: `Welcome ${username}`})
+  //   socket.broadcast.to(room).emmit('message', { user: 'admin', text: `${username} has joined.` })
+  // });
+
+  // socket.on('send_message', (data) => {
+  //   socket.to(data.room).emit('receive_message', data);
+  //   // io.to(data.room).emit('message', data)
+  //   // socket.broadcast.emit('receive_message', data)
+  // });
+
+
 });
 
 // ==== Server start ====
 // server.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
-app.listen(PORT, () =>  console.log(`Server is listening to ${PORT}`))
+server.listen(PORT, () => console.log(`Server is listening to ${PORT}`))
